72e8b319f57081bfe1182ec142cfc10c082d8937
Don-Quixote-Onoda <jmnicolas4me@gmail.com>
2024-02-13 21:31:52 +0800

    updates

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div id="card">
+    <div class="side one">
+        <i class="fas fa-heart"></i>
+    </div>
+    <div class="side two">
+        <p>MI DULCE AMOR,</p>
+        <p>En este día especial, simplemente quiero expresar lo que siento desde lo más profundo de mi corazón. Tú eres mi todo. Cada vez que te veo, siento que el mundo tiene sentido. Eres mi compañer@ de vida, mi apoyo incondicional y mi razón para sonreír cada día.</p>
+        <p>Gracias por estar a mi lado en los buenos y malos momentos, por ser mi confidente y mi mejor amig@. Feliz San Valentín, amor.</p>
+        <h2>TU NOVI@</h2>
+    </div>
+</div>
+  )
+}
+
+export default Card
---
+++ src/Valentines.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Valentines() {
+  return (
+    <div className="valentines-day">
+      <div className="envelope"></div>
+      <div className="heart"></div>
+      <div className="text">HAPPY <br />VALENTINE'S <br />DAY!<br />MY LOVE! </div>
+      <div className="front"></div>
+    </div>
+  )
+}
+
+export default Valentines
